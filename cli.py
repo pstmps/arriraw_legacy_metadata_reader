8753b459c3480fe98f00c854d4151772ccc384a5
pstmps <m.stiebing@gmail.com>
2023-12-19 11:10:13 +0100

    added .spec

--- cli.py
+++ cli.py
@@ -2,19 +2,20 @@
 Command line interface for the arriraw_legacy_metadata_reader package.
 """
 import os
+import sys
 import json
 import click
+from pathlib import Path
+
 import pandas as pd
 
 from arriraw_legacy_metadata_reader.arriraw_legacy_metadata_reader \
     import ArriRawLegacyMetadataReader
 
-
 def validate_json(json_obj, keys):
     # Check which keys are missing in the JSON object
     missing_keys = [key for key in keys if key not in json_obj]
     return missing_keys
-
 
 def load_config(file, keys=[]):
     try:
@@ -25,7 +26,6 @@ def load_config(file, keys=[]):
     if missing_keys:
         print("Invalid config file. Missing keys: ", missing_keys)
     return config
-
 
 def find_files(path, extensions):
     file_found = False
@@ -39,7 +39,6 @@ def find_files(path, extensions):
         raise FileNotFoundError(
             "No files with the given extensions were found.")
 
-
 @click.command()
 @click.option('--inputpath', '-i',
               help='Path to the directory containing the files to be processed.',
@@ -48,7 +47,7 @@ def find_files(path, extensions):
               help='Verbose output.', is_flag=True)
 @click.option('--config', '-c',
               help='Path to the config file.',
-              default='config.json')
+              default='defaultconfig')
 @click.option('--outputpath', '-o',
               help='Path to the output file.If not specified, pwd.')
 @click.option('--format', '-fmt',
@@ -64,10 +63,14 @@ def run(inputpath, verbose, config, outputpath, format, fields):
     if outputpath is None:
         outputpath = os.getcwd()
 
+    if config == 'defaultconfig':
+        script_dir = getattr(sys, '_MEIPASS', os.path.dirname(os.path.realpath(sys.executable)))
+        config = Path(script_dir) / 'config.json'
+
+    click.echo(f'Input path: {config}')
+
     outputfile = os.path.join(outputpath, f'metadata.{format}')
-
     loaded_config = load_config(config, ["supported_files", "defaultfields"])
-
     supported_files = loaded_config["supported_files"]
 
     if fields == 'default':
@@ -81,12 +84,17 @@ def run(inputpath, verbose, config, outputpath, format, fields):
 
     for file in find_files(inputpath.strip('\'').strip('\"'), supported_files):
 
+        click.echo(f'Processing: {file}')
         filename = os.path.splitext(os.path.basename(file))[0]
         arri = ArriRawLegacyMetadataReader(file, fields_to_extract=fields)
         output[filename] = arri.get_dictionary()
 
     if verbose:
-        click.echo(f'Input path: {output}')
+        for clip, value_dict in output.items():
+            click.echo(f'File: {file}')
+            if isinstance(value_dict, dict):
+                for key, value in value_dict.items():
+                    click.echo(f'Key: {key:32} -- Value: {value}')
 
     if format == 'csv':
         df = pd.DataFrame.from_dict(output, orient='index')
@@ -97,7 +105,6 @@ def run(inputpath, verbose, config, outputpath, format, fields):
     else:
         with open(outputfile, 'w') as f:
             json.dump(output, f, indent=4)
-
 
 if __name__ == "__main__":
 
