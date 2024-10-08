be6fc23cce3840d6b5279bf0890f3c97b56e5106
pstmps <m.stiebing@gmail.com>
2024-10-08 22:46:43 +0200

    fixed test directory

--- arriraw_legacy_metadata_reader/tests/test_binaryfiledto.py
+++ arriraw_legacy_metadata_reader/tests/test_binaryfiledto.py
@@ -1,9 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 import pytest
 from arriraw_legacy_metadata_reader.binaryfiledto import BinaryFileDTO
 
 import struct
 import io
-import os
 import binascii
 
 
