875e0c2fad3390ca3a36abe6109bda6e15af7e15
pstmps <m.stiebing@gmail.com>
2024-10-08 16:01:48 +0200

    added readme, refactoring

--- arriraw_legacy_metadata_reader/arriraw_legacy_metadata_reader.py
+++ arriraw_legacy_metadata_reader/arriraw_legacy_metadata_reader.py
@@ -20,34 +20,31 @@ class ArriRawLegacyMetadataReader:
     """
     Class to read the metadata from an ARRIRAW file.
     """
-    def __init__(self, file_path, fields_to_extract=None):
+    def __init__(self, file_path: str, fields_to_extract: Union[list, None] = None):
         try:
             with open(file_path, 'rb') as f:
                 self.rawdata = f.read(4096)
-        except FileNotFoundError as e:
+        except (FileNotFoundError, IOError) as e:
             raise e
         self.fields_to_extract = fields_to_extract
 
-        self.objects = []
+        self.objects = [
+            Idi(file=self.rawdata, fields_to_extract=self.fields_to_extract),
+            Ici(file=self.rawdata, fields_to_extract=self.fields_to_extract),
+            Cdi(file=self.rawdata, fields_to_extract=self.fields_to_extract),
+            Ldi(file=self.rawdata, fields_to_extract=self.fields_to_extract),
+            Vfx(file=self.rawdata, fields_to_extract=self.fields_to_extract),
+            Cid(file=self.rawdata, fields_to_extract=self.fields_to_extract),
+            Sid(file=self.rawdata, fields_to_extract=self.fields_to_extract),
+            Fli(file=self.rawdata, fields_to_extract=self.fields_to_extract),
+            Nri(file=self.rawdata, fields_to_extract=self.fields_to_extract)
+        ]
 
-        self.objects.append(Idi(file=self.rawdata,
-                                fields_to_extract=self.fields_to_extract))
-        self.objects.append(Ici(file=self.rawdata,
-                                fields_to_extract=self.fields_to_extract))
-        self.objects.append(Cdi(file=self.rawdata,
-                                fields_to_extract=self.fields_to_extract))
-        self.objects.append(Ldi(file=self.rawdata,
-                                fields_to_extract=self.fields_to_extract))
-        self.objects.append(Vfx(file=self.rawdata,
-                                fields_to_extract=self.fields_to_extract))
-        self.objects.append(Cid(file=self.rawdata,
-                                fields_to_extract=self.fields_to_extract))
-        self.objects.append(Sid(file=self.rawdata,
-                                fields_to_extract=self.fields_to_extract))
-        self.objects.append(Fli(file=self.rawdata,
-                                fields_to_extract=self.fields_to_extract))
-        self.objects.append(Nri(file=self.rawdata,
-                                fields_to_extract=self.fields_to_extract))
+    def _collect_metadata(self) -> dict:
+        metadata = {}
+        for obj in self.objects:
+            metadata.update(obj.get_data())
+        return metadata
 
     def get_dictionary(self) -> dict:
         """
@@ -55,12 +52,7 @@ class ArriRawLegacyMetadataReader:
         Returns:
             dict: The metadata as a dictionary
         """
-        metadata = {}
-
-        for obj in self.objects:
-            metadata.update(obj.get_data())
-
-        return metadata
+        return self._collect_metadata()
 
     def get_dataframe(self) -> pd.DataFrame:
         """
@@ -68,11 +60,7 @@ class ArriRawLegacyMetadataReader:
         Returns:
             pandas.DataFrame: The metadata as a pandas DataFrame
         """
-        metadata = {}
-
-        for obj in self.objects:
-            metadata.update(obj.get_data())
-
+        metadata = self._collect_metadata()
         return pd.DataFrame.from_dict(metadata, orient='index').transpose()
 
     def get_json(self) -> str:
@@ -90,22 +78,19 @@ class ArriRawLegacyMetadataReader:
             list: A list of all fields in the metadata
         """
         fields = []
-
         for obj in self.objects:
             fields.extend(obj.list_fields())
         return fields
 
-def read_metadata(file_path: str,
-                  fields_to_extract: Union[list, None]=None) -> dict:
+def read_metadata(file_path: str, fields_to_extract: Union[list, None] = None) -> dict:
     """
     Function to read the metadata from an ARRIRAW file.
     Creates a new ArriRawLegacyMetadataReader object and returns the metadata as a dictionary.
     Args:
         file_path (str): The path to the ARRIRAW file
-        fields_to_extract (Union[list, None]): A list of fields to extract. 
+        fields_to_extract (Union[list, None]): A list of fields to extract.
             If None, all fields will be extracted.
     Returns:
         dict: The metadata as a dictionary
     """
-    return ArriRawLegacyMetadataReader(file_path=file_path,
-                                       fields_to_extract=fields_to_extract).get_dictionary()
+    return ArriRawLegacyMetadataReader(file_path=file_path, fields_to_extract=fields_to_extract).get_dictionary()
---
+++ readme.md
@@ -0,0 +1,98 @@
+# ARRI Raw Metadata Reader
+
+A Python package to read legacy ARRI Raw metadata, including a cross-platform demo CLI built with GitHub Actions.
+
+## Features
+
+- Read and parse ARRI Raw metadata
+- Cross-platform command-line interface (CLI)
+- Continuous integration with GitHub Actions
+
+## Usage
+
+### Command Line Interface
+To use the CLI, run the following command - without any options, to enter interactive mode:
+
+```bash
+cli.py
+```
+
+or specify the following options:
+
+```
+    -i, --inputpath TEXT            Path to the directory containing the files
+                                    to be processed.
+    -v, --verbose                   Verbose output.
+    -c, --config TEXT               Path to the config file.
+    -o, --outputpath TEXT           Path to the output file.If not specified,
+                                    pwd.
+    -ofmt, --outputformat [json|csv|xlsx]
+                                    Output format. If not specified, json.
+    -f, --fields [all|minimal|default]
+                                    Fields to extract. If not specified, default
+                                    fields will be extracted.
+    --help                          Show this message and exit.
+```
+
+#### Config file
+
+The default config.json defines options for supported file formats and field configuration:
+
+```json
+{
+    "supported_files" : ["ari", "arx"],
+    "defaultfields" : ["ImageWidth", "ImageHeight", "ActiveImageLeft", "ActiveImageTop", "ActiveImageWidth", "ActiveImageHeight", "FullImageWidth", "FullImageHeigth",
+    "ColorProcessingVersion", "WhiteBalance", "WhiteBalanceCC", "WBFactorR", "WBFactorG", "WBFactorB", "WBAppliedInCamera", "ExposureIndexASA", "TargetColorSpace",
+    "Sharpness", "LensSqueezeFactor", "ImageOrientation", "LookName", "LookLUTMode", "LookLUTOffset", "LookLUTSize", "LookSaturation",
+    "CDLSaturation", "CDLSlopeR", "CDLSlopeG", "CDLSlopeB", "CDLOffsetR", "CDLOffsetG", "CDLOffsetB", "CDLPowerR", "CDLPowerG", "CDLPowerB", "CDLMode",
+    "CameraTypeId", "CameraRevision", "CameraSerialNumber", "CameraId", "CameraIndex", "SystemImageCreationDate", "SystemImageCreationTime", "SystemImageTimeZoneOffset",
+    "SystemImageTimeZoneDST", "ExposureTime", "ShutterAngle", "SensorFPS", "ProjectFPS", "MasterTC", "MasterTCFrameCount", "MasterTCTimeBase",
+    "StorageMediaSerialNumber", "CameraFamily", "RecorderType", "MirrorShutterRunning", "Vari", "UUID", "CameraModel", "CameraProduct", "CameraSubProduct",
+    "LensDistanceUnit", "LensFocusDistance", "LensFocalLength", "LensSerialNumber", "LensLinearIris", "LensIris", "NDFilterType", "NDFilterDensity", "LensModel",
+    "CameraTilt", "CameraRoll", "CircleTake", "Reel", "Scene", "Take", "Director", "Cinematographer", "Production",
+    "ProductionCompany", "VarUserInfoFields", "CameraClipName",
+    "FrameLineFile1", "FrameLineFile2", "FrameLine1A", "FrameLine1B", "FrameLine1C", "FrameLine2A", "FrameLine2B", "FrameLine2C"],
+    "minimal" : ["ImageWidth", "ImageHeight", "ActiveImageLeft", "ActiveImageTop", "ActiveImageWidth", "ActiveImageHeight", "FullImageWidth", "FullImageHeigth",
+    "WhiteBalance", "WhiteBalanceCC", "ExposureIndexASA", "ShutterAngle", "LensSqueezeFactor", "CameraModel", "CameraClipName", "MasterTC"]
+}
+```
+
+
+
+### Python API
+You can also use the package in your Python code:
+
+First, instantiate a new Object:
+
+```python
+from arriraw_legacy_metadata_reader.arriraw_legacy_metadata_reader \
+    import ArriRawLegacyMetadataReader
+
+arri = ArriRawLegacyMetadataReader(file)
+```
+
+Then, get the metadata as either a dictionary, dataframe or json with the methods:
+
+```python
+metadata = arri.get_dictionary()
+
+metadata = arri.get_dataframe()
+
+metadata = arri.get_json()
+```
+
+Alternatively, you can limit the fields extracted by supplying a ```fields_to_extract``` list of metadata fields:
+
+```python
+arri = ArriRawLegacyMetadataReader(file, fields_to_extract=["ImageWidth", "ImageHeight", "CameraClipName"])
+```
+
+
+
+
+
+## Whitepaper
+
+Based on the whitepaper:
+
+https://www.arri.com/resource/blob/31912/e30dd1a324dbecbced88fa146d3f88a9/2020-11-arri-metadata-white-paper-en-data.pdf
